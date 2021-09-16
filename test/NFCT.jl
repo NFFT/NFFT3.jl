c4de5d53aa033226255accd2cca81967807d4ace
TomRiem <riemertom720@googlemail.com>
2021-09-16 09:02:54 +0200

    adds some tests.

--- test/NFCT.jl
+++ test/NFCT.jl
@@ -41,7 +41,7 @@ E_infty = norm(error_vector, Inf) / norm(fhat, 1)
 @test E_2 < 10^(-10)
 @test E_infty < 10^(-10)
 
-# Error tests
+# DomainError tests
 @test_throws DomainError NFCT((-1, 2), M)
 
 @test_throws DomainError NFCT(N, -1)
@@ -56,3 +56,14 @@ E_infty = norm(error_vector, Inf) / norm(fhat, 1)
 @test_throws DomainError NFCT(N, M, (18, 10, 6), Int32(-8), f1_default, f2_default)
 
 @test_throws DomainError NFCT((-16, 8, 4), M, (18, 10, 6), Int32(8), f1_default, f2_default)
+
+# error tests
+
+N = (16, 8, 4)
+M = 10000
+
+p = NFCT(N, M)
+
+p.init_done = false
+
+@test_throws error nfct_finalize_plan(p)
