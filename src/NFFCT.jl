04838d59a064b426d341a1380868a3fb2fb08a07
Pascal Schröter <passscoed@gmail.com>
2022-11-02 16:02:58 +0100

    added NFFCT

---
+++ src/NFFCT.jl
@@ -0,0 +1,241 @@
+using NFFT3
+
+mutable struct NFFCT{D}
+    dcos::NTuple{D,Bool}             # dimensions with cos
+    NFFT_struct::NFFT{D}             # structure for the NFFT
+    function NFFCT{D}(
+        dcos::NTuple{D,Bool},
+        P::NFFT{D}
+    ) where {D}
+        new(dcos, P)
+    end
+end
+
+function NFFCT(
+    dcos::NTuple{D,Bool},
+    N::NTuple{D,Integer},
+    M::Integer,
+    n::NTuple{D,Integer},
+    m::Integer,
+    f1::UInt32 = (D > 1 ? NFFT3.f1_default : NFFT3.f1_default_1d),
+    f2::UInt32 = NFFT3.f2_default,
+) where {D}
+    NFFCT{D}(dcos, NFFT{D}(
+        NTuple{D,Int32}(N),
+        Int32(M),
+        NTuple{D,Int32}(n),
+        Int32(m),
+        (f1 | NFFT3.MALLOC_X | NFFT3.MALLOC_F_HAT | NFFT3.MALLOC_F | NFFT3.FFTW_INIT),
+        f2,
+    ))
+end
+
+# additional constructor for easy use [NFFCT(dcos,N,M) instead of NFFCT{2}(dcos,N,M)]
+function NFFCT(dcos::NTuple{D,Bool},N::NTuple{D,Integer}, M::Integer) where {D}
+    # convert N to vector for passing it over to C
+    Nv = collect(N)
+
+    # default oversampling
+    n = Array{Int32}(2 .^ (ceil.(log.(Nv) / log(2)) .+ 1))
+    n = NTuple{D,Int32}(n)
+
+    # default NFFT flags
+    f1 = UInt32(0)
+
+    if D > 1
+        f1 = NFFT3.f1_default
+    else
+        f1 = NFFT3.f1_default_1d
+    end
+    NFFCT{D}(dcos, NFFT{D}(NTuple{D,Int32}(N), Int32(M), n, Int32(12), f1, NFFT3.f2_default))
+end
+
+@doc raw"""
+    nffct_finalize_plan(P::NFFCT{D})
+
+destroys a NFFT plan structure.
+
+# Input
+* `P` - a NFFCT structure.
+
+# See also
+[`NFFT{D}`](@ref), [`nfft_init`](@ref)
+"""
+function nffct_finalize_plan(P::NFFCT{D}) where {D}
+    nfft_finalize_plan(P.NFFT_struct)
+end
+
+function finalize_plan(P::NFFCT{D}) where {D}
+    return nffct_finalize_plan(P)
+end
+
+# overwrite dot notation for struct to set values and do the transformations at once
+function Base.setproperty!(P::NFFCT{D}, v::Symbol, val) where {D}
+    if v == :x
+        P.NFFT_struct.x = val
+        if D==1
+            if (P.dcos[1])
+                P.NFFT_struct.x ./= 2
+            end
+        else
+            for i in range(1, D)
+                if (P.dcos[i])
+                    P.NFFT_struct.x[i,:] ./= 2
+                end
+            end
+        end
+    elseif v == :fhat
+        a = length(findall(P.dcos))
+        p = prod(P.NFFT_struct.N)
+        l = p÷(2^a)
+        if size(val)[1] != l
+            error("fhat has to be a ComplexFloat64 vector of size prod(N).")
+        end
+        pvExp = zeros(Int64, D)
+        pvExp[D] = 1
+        for i = D-1:-1:1
+            pvExp[i] = pvExp[i+1] * P.NFFT_struct.N[i+1]
+        end
+        pv = zeros(Int64, D)
+        pv[D] = 1
+        for i = D-1:-1:1
+            pv[i] = pv[i+1] * P.NFFT_struct.N[i+1]
+            if (P.dcos[i+1])
+                pv[i] ÷= 2;
+            end
+        end
+        fhatexp = zeros(Complex, p)
+        for i = 1:p
+            idx = 1
+            e = 0;
+            for j = 1:D
+                if (P.dcos[j])
+                    k = abs(((i - 1) ÷ pvExp[j]) % P.NFFT_struct.N[j]-P.NFFT_struct.N[j] ÷ 2)
+                    if k == P.NFFT_struct.N[j] ÷ 2
+                        idx = -1
+                        break
+                    end
+                    if k ≠ 0
+                        e -= 1
+                    end
+                    idx += k * pv[j]
+                else
+                    idx += (((i - 1) ÷ pvExp[j]) % P.NFFT_struct.N[j]) * pv[j]
+                end
+            end
+            if idx == -1
+                fhatexp[i] = 0
+            else
+                fhatexp[i] = sqrt(2)^e * val[idx]
+            end
+        end
+        P.NFFT_struct.fhat = fhatexp
+    elseif v == :f
+        P.NFFT_struct.f = val
+    elseif v == :NFFT_struct
+        @warn "You can't modify the pointer to the NFFT plan."
+    elseif v == :dcos
+        @warn "You can't modify the cosinus dimensions, please create an additional plan."
+    else
+        Base.setproperty!(P.NFFT_struct, v, val)
+    end
+end
+
+# overwrite dot notation to get values from the struct and do the transformations at once
+function Base.getproperty(P::NFFCT{D}, v::Symbol) where {D}
+    if v == :f || v == :plan || v == :num_threads || v == :init_done || v == :N || v == :M || v == :n || v == :m || v == :f1 || v == :f2
+        return Base.getproperty(P.NFFT_struct, v)
+    elseif v == :x
+        x = copy(P.NFFT_struct.x)
+        for i in range(1, D)
+            if (P.dcos[i])
+                x[i,:] .*= 2
+            end
+        end
+        return x
+    elseif v == :fhat
+        a = length(findall(P.dcos))
+        p = prod(P.NFFT_struct.N)
+        l = p÷(2^a)
+        pvExp = zeros(Int64, D)
+        pvExp[D] = 1
+        for i = D-1:-1:1
+            pvExp[i] = pvExp[i+1] * P.NFFT_struct.N[i+1]
+        end
+        pv = zeros(Int64, D)
+        pv[D] = 1
+        for i = D-1:-1:1
+            pv[i] = pv[i+1] * P.NFFT_struct.N[i+1]
+            if (P.dcos[i+1])
+                pv[i] ÷= 2;
+            end
+        end
+        fhat = zeros(Complex, l)
+        fhat_old = P.NFFT_struct.fhat
+        for i = 1:p
+            idx = 1
+            e = 0;
+            for j = 1:D
+                if (P.dcos[j])
+                    k = abs(((i - 1) ÷ pvExp[j]) % P.NFFT_struct.N[j]-P.NFFT_struct.N[j] ÷ 2)
+                    if k == P.NFFT_struct.N[j] ÷ 2
+                        idx = -1
+                        break
+                    end
+                    if k ≠ 0
+                        e -= 1
+                    end
+                    idx += k * pv[j]
+                else
+                    idx += (((i - 1) ÷ pvExp[j]) % P.NFFT_struct.N[j]) * pv[j]
+                end
+            end
+            if idx ≠ -1
+                fhat[idx] += sqrt(2)^e * fhat_old[i]
+            end
+        end
+        return fhat
+    else
+        return Core.getfield(P, v)
+    end
+end
+
+# nffct trafo [call with NFFT.trafo outside module]
+@doc raw"""
+    nffct_trafo(P)
+
+computes the NDFCT via the fast NFFCT algorithm for provided nodes ``\pmb{x}_j, j =1,2,\dots,M,`` in `P.X` and coefficients ``\hat{f}_{\pmb{k}}^c \in \mathbb{R}, \pmb{k} \in I_{\pmb{N},\mathrm{c}}^D,`` in `P.fhat`.
+
+# Input
+* `P` - a NFFCT plan structure.
+
+# See also
+[`NFFCT{D}`](@ref), [`nffct_trafo_direct`](@ref)
+"""
+function nffct_trafo(P::NFFCT{D}) where {D}
+    return nfft_trafo(P.NFFT_struct)
+end
+
+function trafo(P::NFFCT{D}) where {D}
+    return nffct_trafo(P)
+end
+
+# adjoint trafo [call with NFFT.adjoint outside module]
+@doc raw"""
+    nffct_transposed(P)
+
+computes the transposed NDFCT via the fast transposed NFFCT algorithm for provided nodes ``\pmb{x}_j, j =1,2,\dots,M,`` in `P.X` and coefficients ``f_j^c \in \mathbb{R}, j =1,2,\dots,M,`` in `P.f`.
+
+# Input
+* `P` - a NFFCT plan structure.
+
+# See also
+[`NFFCT{D}`](@ref), [`nffct_transposed_direct`](@ref)
+"""
+function nffct_adjoint(P::NFFCT{D}) where {D}
+    return nfft_adjoint(P.NFFT_struct)
+end
+
+function adjoint(P::NFFCT{D}) where {D}
+    return nffct_adjoint(P)
+end
---
+++ test/NFFCT.jl
@@ -0,0 +1,91 @@
+using LinearAlgebra
+using Test
+include("/home/arch/git/NFFT3.jl/src/NFFCT.jl")
+
+function getphi(dcos::NTuple{D,Bool},x::Vector{Float64}, k::Vector{Int64}) where {D}
+    p = 1
+    for i = 1:D
+        if (dcos[i])
+            if k[i] ≠ 0
+                p *= sqrt(2.0)*cos(pi*k[i]*x[i])
+            end
+        else
+            p *= exp(-2.0*pi*im*k[i]*x[i])
+        end
+    end
+    return p
+end
+
+function getMat(N::NTuple{D,Integer},M::Integer, x::Array{Float64},dcos::NTuple{D,Bool}) where {D}
+    a = length(findall(dcos))
+    p = prod(N)
+    n = p÷(2^a)
+    X = copy(x)
+
+    F = [getphi(dcos,X[:, j], getk(dcos,N,l)) for j = 1:M, l = 1:n]
+    return F
+end 
+
+function getk(dcos::NTuple{D,Bool},N::NTuple{D,Integer}, i::Int64) where {D}
+    k = zeros(Int64, D)
+    pv = zeros(Int64, D)
+    pv[D] = 1
+    for i = D-1:-1:1
+        pv[i] = pv[i+1] * N[i+1]
+        if (dcos[i+1])
+            pv[i] ÷= 2;
+        end
+    end
+    for j=1:D
+        if (dcos[j])
+            k[j] = ((i - 1) ÷ pv[j]) % (N[j] ÷ 2)
+        else
+            k[j] = ((i - 1) ÷ pv[j]) % N[j] - N[j] ÷ 2
+        end
+    end
+    return k
+end
+
+N    = (6,   8,     6,     4,    8)
+dcos = (true, false, false, true, true)
+M = 10000
+
+X = rand(5, M)
+a = length(findall(dcos))
+p = prod(N)
+l = p÷(2^a)
+fhat = rand(prod(l)) + im * rand(prod(l))
+
+p = NFFCT(dcos, N, M)
+p.x = X
+p.fhat = fhat
+
+#NFFT3.nffct_trafo(p)
+nffct_trafo(p)
+f2 = p.f
+
+F = getMat(N, M, X,dcos)
+
+f1 = F * fhat
+
+error_vector = f1 - f2
+E_2 = norm(error_vector) / norm(f1)
+E_infty = norm(error_vector, Inf) / norm(fhat, 1)
+
+@test E_2 < 10^(-10)
+@test E_infty < 10^(-10)
+
+#NFFT3.nffct_adjoint(p)
+nffct_adjoint(p)
+f2 = p.fhat
+
+f1 = F' * p.f
+
+error_vector = f1 - f2
+E_2 = norm(error_vector) / norm(f1)
+E_infty = norm(error_vector, Inf) / norm(fhat, 1)
+
+@test E_2 < 10^(-10)
+@test E_infty < 10^(-10)
+
+
