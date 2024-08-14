173575ba1e62941eabaa9a9963ce48def376a8c1
Pascal Schröter <passscoed@gmail.com>
2024-08-14 16:55:44 +0200

    fix format

--- src/NFMT.jl
+++ src/NFMT.jl
@@ -234,7 +234,7 @@ function Base.getproperty(P::NFMT{D}, v::Symbol) where {D}
         pv[D] = 1
         for i = D-1:-1:1
             pv[i] = pv[i+1] * P.NFFT_struct.N[i+1]
-            if (BASES[P.basis_vect[i+1]]>0)
+            if (BASES[P.basis_vect[i+1]] > 0)
                 pv[i] ÷= 2
             end
         end
